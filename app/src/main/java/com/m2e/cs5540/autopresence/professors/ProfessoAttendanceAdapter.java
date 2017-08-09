8c70cc47086f608ef636ed070a17714e6ceabfdd
aswath80 <e.manikandan@gmail.com>
2017-08-08 22:54:48 -0700

    Updated

--- app/src/main/java/com/m2e/cs5540/autopresence/professors/ProfessoAttendanceAdapter.java
+++ app/src/main/java/com/m2e/cs5540/autopresence/professors/ProfessoAttendanceAdapter.java
@@ -14,81 +14,105 @@ import com.m2e.cs5540.autopresence.professors.students.StudentAdapter;
 import com.m2e.cs5540.autopresence.vao.UserAttendance;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Kumar on 8/6/2017.
  */
 
-public class ProfessoAttendanceAdapter extends RecyclerView.Adapter<ProfessoAttendanceAdapter.AttendanceViewHolder>{
+public class ProfessoAttendanceAdapter extends
+      RecyclerView.Adapter<ProfessoAttendanceAdapter.AttendanceViewHolder> {
 
-    private static final String TAG = "ProfAttendanceAdapter";
-    private List<UserAttendance> attendanceList =  new ArrayList<>();
-    private Context context;
+   private static final String TAG = "ProfAttendanceAdapter";
+   private List<UserAttendance> attendanceList = new ArrayList<>();
+   private List<UserAttendance> origAttendanceList = new ArrayList<>();
+   private Context context;
 
-    public ProfessoAttendanceAdapter() {
-    }
+   public ProfessoAttendanceAdapter() {
+   }
 
-    public void setAttendanceList(List<UserAttendance> attendanceList) {
-        this.attendanceList = attendanceList;
-        this.notifyDataSetChanged();
-    }
+   public void setAttendanceList(List<UserAttendance> attendanceList) {
+      this.attendanceList = attendanceList;
+      this.origAttendanceList = new ArrayList<>(attendanceList);
+      this.notifyDataSetChanged();
+   }
 
-    public List<UserAttendance> getAttendanceList() {
-        return attendanceList;
-    }
+   public List<UserAttendance> getAttendanceList() {
+      return attendanceList;
+   }
 
-    @Override
-    public AttendanceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        this.context = parent.getContext();
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View profAttenView = layoutInflater.inflate(R.layout.professor_attendance_view, parent, false);
-        return new AttendanceViewHolder(profAttenView);
-    }
+   @Override public AttendanceViewHolder onCreateViewHolder(ViewGroup parent,
+         int viewType) {
+      this.context = parent.getContext();
+      LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+      View profAttenView = layoutInflater.inflate(
+            R.layout.professor_attendance_view, parent, false);
+      return new AttendanceViewHolder(profAttenView);
+   }
 
-    @Override
-    public int getItemCount() {
-        int itemCount = attendanceList != null ? attendanceList.size() : 0;
-        Log.d(TAG, "$$$ Student attendanceList getItemCount: " + itemCount);
-        return itemCount;
-    }
+   @Override public int getItemCount() {
+      if (attendanceList == null || attendanceList.size() == 0) {
+         return 0;
+      }
+      Set<String> dateSet = new HashSet<>();
+      List<UserAttendance> newList = new ArrayList<>();
+      for (int i = 0; i < attendanceList.size(); i++) {
+         if (dateSet.add(attendanceList.get(i).getAttendanceDate())) {
+            newList.add(attendanceList.get(i));
+         }
+      }
 
-    @Override
-    public void onBindViewHolder(AttendanceViewHolder holder, int position) {
+      Log.i("Manish-Outer", "$$$ newList " + newList);
 
-        UserAttendance userAttendance = attendanceList.get(position);
-        if (userAttendance != null) {
-            holder.setStudentAttendanceDate(userAttendance.getAttendanceDate());
-            Log.i(TAG, "$$$ Done setting student attendance for " +  userAttendance.getAttendanceDate());
-        } else {
-            Log.w(TAG, "$$$ No attendance found for position " + position + " " +  "in " + "adapter ");
-        }
-    }
+      attendanceList = newList;
 
-    class AttendanceViewHolder extends RecyclerView.ViewHolder {
+      Log.d(TAG, "$$$ Student attendanceList getItemCount: " + dateSet.size());
+      return dateSet.size();
+   }
 
-        private static final String TAG = "AttendanceViewHolder";
-        private TextView studentAttendanceDate;
-        private RecyclerView studrecyclerview;
-        private StudentAdapter studentAdapter;
+   @Override
+   public void onBindViewHolder(AttendanceViewHolder holder, int position) {
 
-        public void setStudentAttendanceDate(String studentAttendanceDate) {
-            this.studentAttendanceDate.setText(studentAttendanceDate);
-        }
+      UserAttendance userAttendance = attendanceList.get(position);
+      if (userAttendance != null) {
+         holder.setStudentAttendanceDate(userAttendance.getAttendanceDate());
+         Log.i(TAG, "$$$ Done setting student attendance for " +
+               userAttendance.getAttendanceDate());
+      } else {
+         Log.w(TAG, "$$$ No attendance found for position " + position + " " +
+               "in " + "adapter ");
+      }
+   }
 
-        public AttendanceViewHolder(View itemView) {
-            super(itemView);
-            studentAttendanceDate   = (TextView) itemView.findViewById(R.id.studentAttendanceDate);
-            studrecyclerview        = (RecyclerView) itemView.findViewById(R.id.StudentAttendanceRecyclerView);
+   class AttendanceViewHolder extends RecyclerView.ViewHolder {
 
-            studentAdapter = new StudentAdapter();
-            studentAdapter.setAttendanceList(attendanceList);
-            studrecyclerview.setAdapter(studentAdapter);
-            studentAdapter.notifyDataSetChanged();
-            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(itemView.getContext(),
-                    LinearLayoutManager.VERTICAL, false);
-            studrecyclerview.setLayoutManager(linearLayoutManager);
-        }
+      private static final String TAG = "AttendanceViewHolder";
+      private TextView studentAttendanceDate;
+      private RecyclerView studrecyclerview;
+      private StudentAdapter studentAdapter;
 
-    }
+      public void setStudentAttendanceDate(String studentAttendanceDate) {
+         this.studentAttendanceDate.setText(studentAttendanceDate);
+      }
+
+      public AttendanceViewHolder(View itemView) {
+         super(itemView);
+         studentAttendanceDate = (TextView) itemView.findViewById(
+               R.id.studentAttendanceDate);
+         studrecyclerview = (RecyclerView) itemView.findViewById(
+               R.id.StudentAttendanceRecyclerView);
+
+         studentAdapter = new StudentAdapter();
+         studentAdapter.setDate(studentAttendanceDate.getText().toString());
+         studentAdapter.setAttendanceList(origAttendanceList);
+         studrecyclerview.setAdapter(studentAdapter);
+         studentAdapter.notifyDataSetChanged();
+         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+               itemView.getContext(), LinearLayoutManager.VERTICAL, false);
+         studrecyclerview.setLayoutManager(linearLayoutManager);
+      }
+
+   }
 }
--- app/src/main/java/com/m2e/cs5540/autopresence/professors/students/StudentAdapter.java
+++ app/src/main/java/com/m2e/cs5540/autopresence/professors/students/StudentAdapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.m2e.cs5540.autopresence.R;
-import com.m2e.cs5540.autopresence.students.StudentAttendanceViewHolder;
 import com.m2e.cs5540.autopresence.vao.UserAttendance;
 
 import java.util.List;
@@ -18,41 +17,50 @@ import java.util.List;
 
 public class StudentAdapter extends RecyclerView.Adapter<StudentHolder> {
 
-    private static final String TAG = "StudentAdapter For Professors";
-    private List<UserAttendance> attendanceList;
+   private static final String TAG = "StudentAdapter For Professors";
+   private List<UserAttendance> attendanceList;
+   private String date;
 
-    public StudentAdapter() {
-    }
+   public StudentAdapter() {
+   }
 
-    public void setAttendanceList(List<UserAttendance> attendanceList) {
-        this.attendanceList = attendanceList;
-        this.notifyDataSetChanged();
-    }
+   public void setDate(String date) {
+      this.date = date;
+   }
 
-    @Override
-    public StudentHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View studentView = layoutInflater.inflate(R.layout.professor_student_view, parent, false);
-        return new StudentHolder(studentView);
-    }
+   public void setAttendanceList(List<UserAttendance> attendanceList) {
+      this.attendanceList = attendanceList;
+      this.notifyDataSetChanged();
+   }
 
-    @Override
-    public void onBindViewHolder(StudentHolder holder, int position) {
+   @Override
+   public StudentHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+      View studentView = layoutInflater.inflate(R.layout.professor_student_view,
+            parent, false);
+      return new StudentHolder(studentView);
+   }
 
-        UserAttendance userAttendance = attendanceList.get(position);
-        if (userAttendance != null) {
-            holder.setStudentName(userAttendance.getUserId());
-            holder.setLoggedTime(userAttendance.getAttendanceTime());
-        } else {
-            //Log.w(TAG, "$$$ No attendance found for position " + position + " " + "in " + "adapter ");
-        }
+   @Override public void onBindViewHolder(StudentHolder holder, int position) {
 
-    }
+      UserAttendance userAttendance = attendanceList.get(position);
+      if (userAttendance != null) {
+         holder.setStudentName(userAttendance.getUserId());
+         holder.setLoggedTime(userAttendance.getAttendanceTime());
+      } else {
+         //Log.w(TAG, "$$$ No attendance found for position " + position + " " + "in " + "adapter ");
+      }
 
-    @Override
-    public int getItemCount() {
-        int itemCount = attendanceList != null ? attendanceList.size() : 0;
-        //Log.d(TAG, "$$$ Student attendanceList getItemCount: " + itemCount);
-        return itemCount;
-    }
+   }
+
+   @Override public int getItemCount() {
+      if (attendanceList == null || attendanceList.size() == 0) {
+         return 0;
+      }
+
+      Log.i("Manish-Inner", "$$$ attendanceList " + attendanceList);
+      return attendanceList.size();
+
+      //Log.d(TAG, "$$$ Student attendanceList getItemCount: " + itemCount);
+   }
 }
