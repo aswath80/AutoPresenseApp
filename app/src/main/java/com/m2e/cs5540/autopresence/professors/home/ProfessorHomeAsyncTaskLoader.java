3fcc7f55349435640d748f2e2e3c4977d892a171
aswath80 <e.manikandan@gmail.com>
2017-08-08 22:19:02 -0700

    Updated

--- app/src/main/java/com/m2e/cs5540/autopresence/professors/home/ProfessorHomeAsyncTaskLoader.java
+++ app/src/main/java/com/m2e/cs5540/autopresence/professors/home/ProfessorHomeAsyncTaskLoader.java
@@ -10,6 +10,7 @@ import com.m2e.cs5540.autopresence.database.DatabaseUtil;
 import com.m2e.cs5540.autopresence.exception.AppException;
 import com.m2e.cs5540.autopresence.vao.Course;
 import com.m2e.cs5540.autopresence.vao.CourseAttendancePercent;
+import com.m2e.cs5540.autopresence.vao.CourseEnrollment;
 import com.m2e.cs5540.autopresence.vao.UserAttendance;
 
 import java.text.ParseException;
@@ -22,77 +23,94 @@ import java.util.List;
  * Created by Ekta on 08-08-2017.
  */
 
-public class ProfessorHomeAsyncTaskLoader extends AsyncTaskLoader<AsyncLoaderStatus> {
+public class ProfessorHomeAsyncTaskLoader
+      extends AsyncTaskLoader<AsyncLoaderStatus> {
 
-    private static final String TAG = "ProfHomeAsyncTaskLoader";
-    private static final SimpleDateFormat meetingDateFormat = new
-          SimpleDateFormat("dd-MMM-yyyy");
+   private static final String TAG = "ProfHomeAsyncTaskLoader";
+   private static final SimpleDateFormat meetingDateFormat =
+         new SimpleDateFormat("dd-MMM-yyyy");
 
-    public ProfessorHomeAsyncTaskLoader(Context context) {
-        super(context);
-    }
+   public ProfessorHomeAsyncTaskLoader(Context context) {
+      super(context);
+   }
 
-    @Override
-    public AsyncLoaderStatus loadInBackground() {
-        AsyncLoaderStatus loaderStatus = new AsyncLoaderStatus();
-        try {
-            List<Course> coursesList = DatabaseUtil.getInstance().getCoursesByProfId( AppContext.getCurrentAppContext().getUser().getId());
-            Log.i(TAG, "Professor course registered size is: " + coursesList.size());
-            if (coursesList != null) {
-                List<CourseAttendancePercent> courseAttendancePercentList = new ArrayList<>();
-                for (int i = 0; i < coursesList.size(); i++) {
-                    CourseAttendancePercent courseAttendancePercent =  new CourseAttendancePercent();
+   @Override public AsyncLoaderStatus loadInBackground() {
+      AsyncLoaderStatus loaderStatus = new AsyncLoaderStatus();
+      try {
+         List<Course> coursesList =
+               DatabaseUtil.getInstance().getCoursesByProfId(
+                     AppContext.getCurrentAppContext().getUser().getId());
+         Log.i(TAG,
+               "Professor course registered size is: " + coursesList.size());
+         if (coursesList != null) {
+            List<CourseAttendancePercent> courseAttendancePercentList =
+                  new ArrayList<>();
+            for (int i = 0; i < coursesList.size(); i++) {
+               CourseAttendancePercent courseAttendancePercent =
+                     new CourseAttendancePercent();
 
-                    Course course = coursesList.get(i);
-                    //Course course = DatabaseUtil.getInstance().getCourse(course.getCourseId());
-                    courseAttendancePercent.setCourseCount(coursesList.size());
-                    courseAttendancePercent.setCourseName(course.getName());
-                    courseAttendancePercent.setAttendancePercent(String.format("%.2f %%", calculateAttendancePercent(course)));
-                    courseAttendancePercentList.add(courseAttendancePercent);
-                }
-                loaderStatus.setResult(courseAttendancePercentList);
+               Course course = coursesList.get(i);
+               //Course course = DatabaseUtil.getInstance().getCourse(course.getCourseId());
+               courseAttendancePercent.setCourseCount(coursesList.size());
+               courseAttendancePercent.setCourseName(course.getName());
+               courseAttendancePercent.setAttendancePercent(
+                     String.format("%.2f %%",
+                           calculateAttendancePercent(course)));
+               courseAttendancePercentList.add(courseAttendancePercent);
             }
-        } catch (Exception e) {
+            loaderStatus.setResult(courseAttendancePercentList);
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+         loaderStatus.setException(e);
+      }
+      return loaderStatus;
+   }
+
+   private double calculateAttendancePercent(Course course) {
+
+      Log.i(TAG, "$$$ Calculating attendance percent for course " + course);
+      List<CourseEnrollment> courseEnrollmentList =
+            DatabaseUtil.getInstance().getCourseEnrollmentsByCourseId(
+                  course.getId());
+      List<UserAttendance> userAttendanceList =
+            DatabaseUtil.getInstance().getCourseAttendances(course.getId());
+      if (courseEnrollmentList != null && courseEnrollmentList.size() > 0 &&
+            userAttendanceList != null && userAttendanceList.size() > 0) {
+         String meetingStartDate = course.getMeetingDate().getStartDate();
+         try {
+            Date fromDate = meetingDateFormat.parse(meetingStartDate);
+            long msDiff = new Date().getTime() - fromDate.getTime();
+            long daysDiff = msDiff / (1000 * 60 * 60 * 24L);
+            int classDaysInAWeek = calculateNoOfClassDays(
+                  course.getMeetingDate().getMeetingDays());
+            double classDays = (double) daysDiff / classDaysInAWeek;
+            Log.i(TAG,
+                  "$$$ msDiff = " + msDiff + " daysDiff = " + daysDiff + " " +
+                        "classDaysInAWeek = " + classDaysInAWeek);
+            return (userAttendanceList.size() * 100) / classDays /
+                  courseEnrollmentList.size();
+         } catch (ParseException e) {
             e.printStackTrace();
-            loaderStatus.setException(e);
-        }
-        return loaderStatus;
-    }
+            throw new AppException(
+                  "Error parsing meeting date " + meetingStartDate);
+         }
+      }
+      return 0;
+   }
 
-    private double calculateAttendancePercent(Course course) {
+   private int calculateNoOfClassDays(String meetingDays) {
+      int count = 0;
+      char[] cs = meetingDays.toCharArray();
+      for (char c : cs) {
+         if (c == '1') {
+            count++;
+         }
+      }
+      return count;
+   }
 
-        Log.i(TAG, "$$$ Calculating attendance percent for course " + course);
-        List<UserAttendance> userAttendanceList = DatabaseUtil.getInstance().getCourseAttendances(course.getId());
-        if (userAttendanceList != null && userAttendanceList.size() > 0) {
-            String meetingStartDate = course.getMeetingDate().getStartDate();
-            try {
-                Date fromDate = meetingDateFormat.parse(meetingStartDate);
-                long msDiff = new Date().getTime() - fromDate.getTime();
-                long daysDiff = msDiff / (1000 * 60 * 60 * 24L);
-                int classDaysInAWeek = calculateNoOfClassDays(course.getMeetingDate().getMeetingDays());
-                double classDays = (double) daysDiff / classDaysInAWeek;
-                Log.i(TAG, "$$$ msDiff = " + msDiff + " daysDiff = " + daysDiff + " " + "classDaysInAWeek = " + classDaysInAWeek);
-                return userAttendanceList.size() * 100 / classDays;
-            } catch (ParseException e) {
-                e.printStackTrace();
-                throw new AppException("Error parsing meeting date " + meetingStartDate);
-            }
-        }
-        return 0;
-    }
-
-    private int calculateNoOfClassDays(String meetingDays) {
-        int count = 0;
-        char[] cs = meetingDays.toCharArray();
-        for (char c : cs) {
-            if (c == '1') {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    @Override public void deliverResult(AsyncLoaderStatus data) {
-        super.deliverResult(data);
-    }
+   @Override public void deliverResult(AsyncLoaderStatus data) {
+      super.deliverResult(data);
+   }
 }
